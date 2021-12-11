9f9ea6f3fde47bdb62d2903e1c8ebdaa43146424
pee <leonardo.price@gmail.com>
2021-12-10 23:34:42 -0500

    ingredients and recipe data

---
+++ gross food/Assets/Manager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public GameObject[] ingredients;
+    public GameObject[] recipes;
+    public string[] ingredientNames;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ingredientData();
+
+        recipeData();
+        
+    }
+
+    void ingredientData()
+    {
+        ingredients = GameObject.FindGameObjectsWithTag("ingredient");
+        ingredientNames = new string[ingredients.Length];
+
+        int counter = 0;
+        foreach (GameObject ingredientName in ingredients)
+        {
+            ingredientNames[counter] = ingredientName.GetComponent<ingredient>().name;
+
+            counter++;
+        }
+    }
+
+    void recipeData()
+    {
+        recipes = GameObject.FindGameObjectsWithTag("recipe");
+        foreach (GameObject RecipeObj in recipes)
+        {
+            GameObject[] RecipeIngredientObj = RecipeObj.GetComponent<Recipe>().recipeIngredients;
+           foreach (GameObject ingredientChild in RecipeIngredientObj)
+           {
+                string name = ingredientChild.GetComponent<ingredient>().name;
+                Debug.Log(name);
+           }
+
+        }
+
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ gross food/Assets/objectSelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectSelect : MonoBehaviour
+{
+
+    Ray ray;
+    RaycastHit hit;
+
+    ingredient ingredientScript;
+
+    public Text ingredientNameTxt;
+    public Text descriptionTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("piss!");
+
+            if (hit.transform.gameObject.GetComponent<ingredient>() != null)
+            {
+                ingredientScript = hit.transform.gameObject.GetComponent<ingredient>();
+                ingredientNameTxt.text = ingredientScript.name;
+                descriptionTxt.text = ingredientScript.Discription;
+
+
+            }
+
+        }
+
+
+    }
+
+
+    
+
+}
